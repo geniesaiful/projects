6ee4ff5306e373acf991fa653118f4a3cf5a8f83
geniesaiful <genie.saiful@gmail.com>
2026-07-26 11:19:27 +0600

    Left menu div selector done

--- ExpTracker/scripts.js
+++ ExpTracker/scripts.js
@@ -1,3 +1,8 @@
-function showRigtContent(section){
-    console.log("HELLOW WORLD");
+function showRigtContent(sectionName){
+    const allSections = document.querySelectorAll('.contentDiv');
+        for (let i = 0; i < allSections.length; i++) {
+            allSections[i].classList.remove('activeDiv');  
+        }
+    const selectedSection = document.getElementById(sectionName);
+    selectedSection.classList.add('activeDiv');
 }
