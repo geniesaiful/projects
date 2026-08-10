5651434a92e402373bcfbe918deb70750472da5c
Mohammad Saiful Islam <genie.saiful@gmail.com>
2026-08-11 01:44:45 +0600

    Logic plan. stable

--- QuizApp/scripts.js
+++ QuizApp/scripts.js
@@ -1,1 +1,18 @@
-console.log("hellow World");
+
+
+function beginQuiz(){
+    /*reset timer*/
+    displayContainer('quizPage');
+    /*start timer*/
+}
+
+function displayContainer(container){
+    console.log("hellow World");
+    const allActive = document.querySelectorAll('.activated');
+    console.log(allActive);
+    for(let i=0;i<allActive.length;i++){
+        allActive[i].classList.remove('activated');
+    }
+    document.getElementById(container).classList.add('activated');
+}
+
