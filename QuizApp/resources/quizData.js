c3da718803172ed7ee69dceacfc597f2c1195821
geniesaiful <genie.saiful@gmail.com>
2026-08-11 11:07:52 +0600

    Data loaded. button issue.

---
+++ QuizApp/resources/quizData.js
@@ -0,0 +1,46 @@
+const quizData = [
+    {
+        id: 1,
+        question: "What is the real name of Tony Stark's superhero alter ego?",
+        choices: [
+            "Captain America",
+            "Iron Man",
+            "Thor",
+            "Black Panther"
+        ],
+        correctAnswer: 1 
+    },
+    {
+        id: 2,
+        question: "What metal is Captain America's shield made of?",
+        choices: [
+            "Adamantium",
+            "Titanium",
+            "Vibranium",
+            "Kryptonite"
+        ],
+        correctAnswer: 2 
+    },
+    {
+        id: 3,
+        question: "What is the home realm of Thor?",
+        choices: [
+            "Asgard",
+            "Wakanda",
+            "Xandar",
+            "Sakaar"
+        ],
+        correctAnswer: 0 
+    },
+    {
+        id: 4,
+        question: "How many Infinity Stones are there in total in the Marvel Cinematic Universe?",
+        choices: [
+            "4",
+            "5",
+            "6",
+            "7"
+        ],
+        correctAnswer: 2 
+    }
+];
--- QuizApp/scripts.js
+++ QuizApp/scripts.js
@@ -1,13 +1,32 @@
-
+let quizPageNo,quizId,quizQuestion,quizChoice,quizAnswer;
 
 function beginQuiz(){
+    quizId = 1;
+    loadQuiz(quizId);
     /*reset timer*/
     displayContainer('quizPage');
     /*start timer*/
 }
+function initQuiz(){
+    quizId = 1;
+    quizQuestion = quizData[0].question;
+    quizChoice = quizData[0].choices;
+    quizAnswer = quizData[0].correctAnswer;
+}
+function loadQuiz(id){
+    quizId = id;
+    quizQuestion = quizData[quizId-1].question;
+    quizCoice = quizData[quizId-1].choices;
+    quizAnswer = quizData[quizId-1].correctAnswer;
+    console.log(quizQuestion+"\n"+quizCoice+"\n"+quizAnswer);
 
+    document.getElementById("questionText").textContent = quizQuestion;
+    for(let i=0;i<quizCoice.length;i++){
+        /*console.log(document.querySelectorAll('.choice-text')[i].textContent);*/
+        document.querySelectorAll('.choice-text')[i].textContent = quizCoice[i];
+    }
+}
 function displayContainer(container){
-    
     const allActive = document.querySelectorAll('.activated');
     /*console.log(allActive);*/
     for(let i=0;i<allActive.length;i++){
@@ -15,9 +34,20 @@ function displayContainer(container){
     }
     document.getElementById(container).classList.add('activated');
 }
-
 function changePage(){
-    console.log("hellow World");
-    /* If the quiz questions then locaQuestions() else */
-    displayContainer('reviewPage');
+    const btndis = document.getElementById('previous');
+    btndis.disabled = true;
+    if(quizId<quizData.length){
+        console.log(quizId+"\n"+quizData.length);
+        quizId++;
+        loadQuiz(quizId);
+        if((quizId)==quizData.length){
+            document.getElementById('previous').disabled=true;
+            document.getElementById('previous').textContent="crazytown";
+        }
+    }
+    else{
+        displayContainer('reviewPage');
+    }
+    
 }
