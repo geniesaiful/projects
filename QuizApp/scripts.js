ce6d93613ce176c352c6eb285c36015f4bc29c39
geniesaiful <genie.saiful@gmail.com>
2026-08-11 13:49:05 +0600

    issues fixed, calculation done. stable.

--- QuizApp/scripts.js
+++ QuizApp/scripts.js
@@ -1,9 +1,22 @@
 let quizPageNo,quizId,quizQuestion,quizChoice,quizAnswer;
+let userChoice;
+let answerArray = Array(quizData.length);
+let correct,incorrect,incomplete;
 
 function beginQuiz(){
     quizId = 1;
+    correct = 0;
+    incorrect = 0;
+    incomplete = 0;
     loadQuiz(quizId);
     /*reset timer*/
+    userChoice = Array(quizData.length).fill(-1);
+    for(let i=0;i<quizData.length;i++){
+        answerArray[i]=quizData[i].correctAnswer;
+        //console.log(answerArray[i]);
+    }
+    //console.log(answerArray);
+
     displayContainer('quizPage');
     /*start timer*/
     if(document.getElementById('next').textContent==="Finish Quiz"){
@@ -20,22 +33,29 @@ function loadQuiz(id){
     /* clear radio buttons each time. will change later*/
     const allRadioBtn = document.querySelectorAll('input[name="quiz-choice"]');
     for(let i=0;i<allRadioBtn.length;i++){
-        console.log(allRadioBtn[i].checked);
+        //console.log(allRadioBtn[i].checked);
         allRadioBtn[i].checked=false;
     }
-
-
     quizId = id;
     quizQuestion = quizData[quizId-1].question;
     quizCoice = quizData[quizId-1].choices;
     quizAnswer = quizData[quizId-1].correctAnswer;
-    // console.log(quizQuestion+"\n"+quizCoice+"\n"+quizAnswer);
+    //console.log(quizQuestion+"\n"+quizCoice+"\n"+quizAnswer);
 
     document.getElementById("questionText").textContent = quizQuestion;
     for(let i=0;i<quizCoice.length;i++){
         //console.log(document.querySelectorAll('.choice-text')[i].textContent);
         document.querySelectorAll('.choice-text')[i].textContent = quizCoice[i];
     }
+    // take the user choice
+    for(let i=0;i<allRadioBtn.length;i++){
+        //console.log("inside loop");
+        allRadioBtn[i].addEventListener("change",function(event){
+            userChoice[quizId-1] = event.target.value;
+            //console.log(userChoice);
+        });
+    }
+
 }
 function displayContainer(container){
     const allActive = document.querySelectorAll('.activated');
@@ -46,7 +66,7 @@ function displayContainer(container){
     document.getElementById(container).classList.add('activated');
 }
 function nextPage(){
-    if(document.getElementById('previous').disabled=true){
+    if(document.getElementById('previous').disabled){
         document.getElementById('previous').disabled=false;
     }
     if(quizId<quizData.length){
@@ -60,6 +80,7 @@ function nextPage(){
     }
     else{
         displayContainer('reviewPage');
+        calResult();
     }
 }
 function prevPage(){
@@ -73,4 +94,21 @@ function prevPage(){
         loadQuiz(quizId);
     }
     loadQuiz(quizId);
+}
+function calResult(){
+    console.log("calResult");
+    for(let i=0;i<answerArray.length;i++){
+        if(userChoice[i] == answerArray[i]){
+            correct++;
+        }
+        if(userChoice[i]==-1){
+            incomplete++;
+        }
+        if(userChoice[i]!=-1 && userChoice[i]!=answerArray[i]){
+            incorrect++;
+        }
+    }
+    console.log("correct: "+correct+" incorrect: "+incorrect+" incomplete: "+incomplete);
+    console.log(userChoice+"\t"+answerArray);
+    
 }
