0d5fb74f71c0775278dedda8b1424493e944ad28
geniesaiful <genie.saiful@gmail.com>
2026-08-22 17:53:45 +0600

    Parsing from apk data done.

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,4 +1,6 @@
 getWeatherData('Dhaka');
+
+
 async function getWeatherData(city) {
     
     const apiKey = '4c4bfb50e8aa436c81193431262208';
@@ -14,13 +16,14 @@ async function getWeatherData(city) {
         console.log(data);
         const city=data.location.name;
         const tempC = data.current.temp_c;
-
-        console.log(city,tempC);
+        document.getElementById('mdDateTime').innerHTML=`${data.current.last_updated}`;
+        document.getElementById('mdLocation').innerHTML=`${data.location.name}, ${data.location.country}`;
         document.getElementById('resultT').innerHTML=`${tempC}`;
 
     }catch(error){
-        document.getElementById('resultP').innerHTML = error.message;
+        document.getElementById('resultT').innerHTML = error.message;
 
     }
 
 }
+//console.log("This is still executed because of try catch?");
