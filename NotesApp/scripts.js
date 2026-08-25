0f6e618b8362756bda3ed8a00cb7d48811e6b65d
geniesaiful <genie.saiful@gmail.com>
2026-08-25 15:21:00 +0600

    notes preliminary design done.

--- NotesApp/scripts.js
+++ NotesApp/scripts.js
@@ -14,6 +14,9 @@ function updateNoteCard(note){
     document.getElementById('ndTopDateTimeID').textContent = dateFormatter(note.updatedAt);
     document.getElementById('ndCatID').textContent = note.category;
     document.getElementById('ndTagID').textContent = note.tags;
+    document.getElementById('ndDescTxtID').textContent = note.content;
+    document.getElementById('ndCreatedDateID').textContent = dateFormatter(note.createdAt);
+    document.getElementById('ndModifiedDateID').textContent = dateFormatter(note.updatedAt);
 }
 
 function dateFormatter(rawDate){
