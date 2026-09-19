df30097feec918349a20202efe07185f48a3c062
Mohammad Saiful Islam <genie.saiful@gmail.com>
2026-09-19 16:53:59 +0600

    usestate example in todo app

--- nodeTryout/my-todo-app/src/App.jsx
+++ nodeTryout/my-todo-app/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
 
   const [taskInput, setTaskInput] = useState('');
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem('RctTodoApp_Tasks');
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+  const [filter, setFilter] = useState('all');
 
   const handleAddTask = () => {
     if(taskInput.trim() === ''){
@@ -16,8 +21,10 @@ function App() {
       text: taskInput,
       checked: false
     };
-  
-    setTasks([...tasks, newTask]);  
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);  
+
+    localStorage.setItem('RctTodoApp_Tasks',JSON.stringify(updatedTasks));
     setTaskInput(''); 
   }
   const handleToggleTask = (taskIdtoToggle) => {
@@ -29,41 +36,77 @@ function App() {
       });
 
     setTasks(updatedTasks);     
+    localStorage.setItem('RctTodoApp_Tasks',JSON.stringify(updatedTasks));
   };
 
 
+  const totalTasks = tasks.length;
+  const doneTasks = tasks.filter((task)=> task.checked).length;
+  const remainingTasks = totalTasks - doneTasks;
+
+  const filteredTasks = tasks.filter((task) => {
+      if( filter === 'completed') return task.checked;
+      if( filter === 'remaining') return !task.checked;
+      return true;
+  });
+
   return(
-    <div style={{padding: '2rem', fontFamily: 'ariel' }}>
+    <div className="mainContainer" style={{padding: '2rem', fontFamily: 'arial' }}>
       <h3>TO-DO List</h3>
-      
-      
-      
-      <div>
+      <div className="inputContainer">
         <input 
         type="text"
         placeholder="Write the task"
         value={taskInput}
+        className="task-input"
         onChange={(e) => setTaskInput(e.target.value)}
         />
-        <button onClick={handleAddTask}>Add</button>
+        <button className="add-btn" onClick={handleAddTask}>Add</button>
 
       </div>
 
-      <ul style={{padding: '1rem', listStyle: 'none', marginTop: '.2rem'}}>
-        {tasks.map((task)=> (
-          <li key={task.id}>
-            <input 
-              type="checkbox"
-              checked={task.checked}
-              onChange={()=> handleToggleTask(task.id)}
-            />
-            <span>
-              {task.text}
-            </span>
-          </li>
-        ))}
+      <div className="statBar">
+        <span>Total: {totalTasks}</span>
+        <span>Done: {doneTasks}</span>
+        <span>Remaining: {remainingTasks}</span>
+      </div>
 
-      </ul>
+      <div className='filterBar'>
+        <button className={`filter-btn ${filter === 'all' ? 'active' : ''}`}
+        onClick={() => setFilter('all')}>
+          All
+        </button>
+        <button className={`filter-btn ${filter === 'completed' ? 'active' : ''}`}
+        onClick={() => setFilter('completed')}>
+          Completed
+        </button>
+        <button className={`filter-btn ${filter === 'remaining' ? 'active' : ''}`}
+        onClick={() => setFilter('remaining')}>
+          Reamaining
+        </button>
+      
+      </div>
+   
+      <div className="tasksContainer">
+        <ul className="taksList">
+          {filteredTasks.map((task)=> (
+            <li key={task.id}>
+              <input 
+                type="checkbox"
+                checked={task.checked}
+                onChange={()=> handleToggleTask(task.id)}
+              />
+              <span style={{ 
+                marginLeft: '.5rem', 
+                textDecoration: task.checked ? 'line-through' : 'none',
+              }}>
+                {task.text}
+              </span>
+            </li>
+          ))}
+
+        </ul>
+      </div>
     </div>
     
   );
